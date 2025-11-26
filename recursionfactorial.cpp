fb4b3d9328f7c2669ff887b51698a94be1669964
meena615 <malladimeenakshi9@gmail.com>
2025-11-26 11:54:59 +0530

    Add files via upload
    hi

---
+++ recursionfactorial.cpp
@@ -0,0 +1,18 @@
+/*write a c program to find factorial of a given number using recursion*/
+#include<stdio.h>
+int fact(int);
+int main()
+{
+	int n;
+	printf("Enter any +ve integer number");
+	scanf("%d",&n);
+	
+	printf("factorial is %d",fact(n));
+}
+int fact(int x)
+{
+	if(x == 1)
+	  return x;
+	else
+	  return x*fact(x-1);
+}
